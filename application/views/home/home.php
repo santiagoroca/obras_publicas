e7a82e0adca290ec0f7647b6f55f9015b217ea12
unknown <bustamantepaula@outlook.com>
2015-07-04 17:48:08 -0300

    vista log in

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,31 +1,22 @@
-<div class="row">
+<div class="row log-in">
   <div class="col-md-4"></div>
-  <div class="col-md-4 log-in-form">
-    <form class="form-horizontal">
+  <div class="col-md-4 form">
+    <form class="form-horizontal" action="<?=$action_url?>" method="POST">
       <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
         <div class="col-sm-10">
-          <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+          <input name="usuario" type="text" class="form-control" placeholder="Email">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+        <label for="inputPassword3" class="col-sm-2 control-label">Contraseña</label>
         <div class="col-sm-10">
-          <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+          <input name="contrasenia" type="password" class="form-control" placeholder="Password">
         </div>
       </div>
       <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Remember me
-            </label>
-          </div>
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default">Sign in</button>
+          <button type="submit" class="btn btn-default">Ingresar</button>
         </div>
       </div>
     </form>
