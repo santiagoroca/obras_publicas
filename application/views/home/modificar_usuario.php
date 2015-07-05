f96887a74cfbac7a0ebffe256cb9f57be4a8f30c
unknown <bustamantepaula@outlook.com>
2015-07-05 18:09:58 -0300

    update user

--- application/views/home/modificar_usuario.php
+++ application/views/home/modificar_usuario.php
@@ -1,64 +1,72 @@
-<div class="topbar">
-</div>
-<nav class="navbar navbar-inverse">
-  <button type="button" class="btn btn-default navbar-btn">Sign in</button>
-</nav>
+
 <div class="row">
-  <div class="col-md-2"></div>
-  <div class="col-md-8">
-    <div class="row">
-      <div class="col-md-3">
-        <div class="form-group">
-          <label >Usuario</label>
-          <input name="usuario" class="form-control" placeholder="Usuario" required>
+  <div class="col-md-3"></div>
+  <div class="col-md-6">
+    <h1 class="update-user-title" >Mis Datos</h1>
+    <form method="POST" action="">
+      <div class="row">
+        <div class="col-md-5">
+          <div class="form-group">
+            <label >Usuario</label>
+            <input name="usuario" class="form-control" placeholder="Usuario" required>
+          </div>
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-5">
+          <div class="form-group">
+            <label >Contraseña</label>
+            <input name="contrasenia" type="password" class="form-control" placeholder="Contraseña" required>
+          </div>
         </div>
       </div>
-      <div class="col-md-2"></div>
-      <div class="col-md-3">
-        <div class="form-group">
-          <label >Contraseña</label>
-          <input name="contrasenia" type="password" class="form-control" placeholder="Contraseña" required>
+      <div class="row">
+        <div class="col-md-offset-10 col-md-2">
+          <div class="form-group">
+            <button type="submit" class="btn btn-default">Guardar</button>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class=""></div>
-      <div class="col-md-offset-7 col-md-3">
-        <div class="form-group">
-          <button type="submit" class="btn btn-default">Guardar</button>
-        </div>
-      </div>
-    </div>
-
+    </form>
     <div class="page-header page-header-custom" ></div>
-    
     <div class="row">
-      <div class="col-md-3">
-        <div class="form-group">
-          <label >Nombre</label>
-          <input name="nombre" class="form-control" placeholder="Nombre" required>
+      <form method="POST" action="">
+        <div class="col-md-5">
+          <div class="form-group">
+            <label >Nombre</label>
+            <input name="nombre" class="form-control" placeholder="Nombre" required>
+          </div>
+          <div class="form-group">
+            <label >Fecha de Nacimiento</label>
+            <input name="fechadenacimiento" class="form-control" placeholder="Fecha de Nacimiento" required>
+          </div>
+          <div class="form-group">
+            <label >Teléfono</label>
+            <input name="telefono" class="form-control" placeholder="Teléfono" required>
+          </div>
         </div>
-      </div>
-      <div class="col-md-2"></div>
-      <div class="col-md-3">
-        <div class="form-group">
-          <label >Apellido</label>
-          <input name="apellido" type="password" class="form-control" placeholder="Apellido" required>
+        <div class="col-md-2"></div>
+        <div class="col-md-5">
+          <div class="form-group">
+            <label >Apellido</label>
+            <input name="apellido" type="password" class="form-control" placeholder="Apellido" required>
+          </div>
+          <div class="form-group">
+            <label >Dirección</label>
+            <input name="direccion" class="form-control" placeholder="Dirección" required>
+          </div>
+          <div class="form-group">
+            <label >Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Email" required>
+          </div>
         </div>
-      </div>
+        <div class="row">
+        <div class="col-md-offset-10 col-md-1">
+          <div class="form-group">
+            <button type="submit" class="btn btn-default">Guardar</button>
+          </div>
+        </div>
+      </form>
     </div>
-  </div>
-  <div class="col-md-2">
-    <?php
-    if ($errors)
-    foreach ($errors as $k => $v) {
-    ?>
-    <div class="alert alert-danger" role="alert"><?=$v?></div>
-    <?php
-    }
-    ?>
-    <div class="alert alert-danger" role="alert"></div>
   </div>
 </div>
 <!--<div class="col-md-2 form-createuser">
