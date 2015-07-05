12c18192f299d4af2254057ac2f02cd7e370f9a7
unknown <bustamantepaula@outlook.com>
2015-07-04 23:22:17 -0300

    User CRU

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,25 +1,29 @@
 <div class="row log-in">
-  <div class="col-md-4"></div>
-  <div class="col-md-4 form-createuser">
+  <div class="col-md-5"></div>
+  <div class="col-md-2 form-createuser">
+    <h2 class="form-create-user-title" >Iniciar Sesion</h2>
+    <div class="page-header page-header-custom" ></div>
     <form class="form-horizontal" action="<?=$action_url?>" method="POST">
       <div class="form-group">
-        <label for="inputEmail3" class="col-sm-2 control-label">Usuario</label>
-        <div class="col-sm-10">
+        <div class="col-sm-12">
           <input name="usuario" type="text" class="form-control" placeholder="Email">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputPassword3" class="col-sm-2 control-label">Contraseña</label>
-        <div class="col-sm-10">
+        <div class="col-sm-12">
           <input name="contrasenia" type="password" class="form-control" placeholder="Password">
         </div>
       </div>
-      <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default">Ingresar</button>
+      <div class="row-fluid">
+        <div class="col-md-8">
+         <a href="<?=$new_user_url?>">Nuevo Usuario</a>
         </div>
-      </div>
-    </form>
+        <div class="col-md-4">
+          <div class="form-group">
+            <button type="submit" class="btn btn-default">Ingresar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-4"></div>
   </div>
-  <div class="col-md-4"></div>
-</div>
--- application/views/home/nuevo_usuario.php
+++ application/views/home/nuevo_usuario.php
@@ -42,8 +42,7 @@
         <input name="email" class="form-control" placeholder="Email">
       </div>
       <div class="form-group">
-        <button type="submit" class="btn btn-default">Registrar</button>
-        <div style="clear:both"></div>
+        <button type="submit" class="btn btn-default pull-right">Registrar</button>
       </div>
     </form>
   </div>
