bc5942255efc73e64512b0090b5ecdd96b720104
unknown <bustamantepaula@outlook.com>
2015-07-05 19:51:43 -0300

    top bar

--- application/views/home/modificar_usuario.php
+++ application/views/home/modificar_usuario.php
@@ -1,4 +1,3 @@
-
 <div class="row">
   <div class="col-md-3"></div>
   <div class="col-md-6">
@@ -48,7 +47,7 @@
         <div class="col-md-5">
           <div class="form-group">
             <label >Apellido</label>
-            <input name="apellido" type="password" class="form-control" placeholder="Apellido" required>
+            <input name="apellido" type="text" class="form-control" placeholder="Apellido" required>
           </div>
           <div class="form-group">
             <label >Dirección</label>
@@ -60,46 +59,46 @@
           </div>
         </div>
         <div class="row">
-        <div class="col-md-offset-10 col-md-1">
-          <div class="form-group">
-            <button type="submit" class="btn btn-default">Guardar</button>
+          <div class="col-md-offset-10 col-md-1">
+            <div class="form-group">
+              <button type="submit" class="btn btn-default">Guardar</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
-</div>
-<!--<div class="col-md-2 form-createuser">
-  <form>
-    <div class="form-group">
-      <label >Usuario</label>
-      <input name="usuario" class="form-control" placeholder="Usuario">
-    </div>
-    <div class="form-group">
-      <label >Contraseña</label>
-      <input name="contrasenia" type="password" class="form-control" placeholder="Contraseña">
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-default">Registrar</button>
-      <div style="clear:both"></div>
-    </div>
-    <div class="page-header page-header-custom" ></div>
-    <div class="form-group">
-      <input name="nombre" class="form-control" placeholder="Nombre">
-    </div>
-    <div class="form-group">
-      <input name="apellido" class="form-control" placeholder="Apellido">
-    </div>
-    <div class="form-group">
-      <input name="direccion" class="form-control" placeholder="Dirección">
-    </div>
-    <div class="form-group">
-      <input name="telefono" class="form-control" placeholder="Teléfono">
-    </div>
-    <div class="form-group">
-      <input name="email" class="form-control" placeholder="Email">
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-default pull-right">Registrar</button>
-    </div>
-  </form>-->
+  <!--<div class="col-md-2 form-createuser">
+    <form>
+      <div class="form-group">
+        <label >Usuario</label>
+        <input name="usuario" class="form-control" placeholder="Usuario">
+      </div>
+      <div class="form-group">
+        <label >Contraseña</label>
+        <input name="contrasenia" type="password" class="form-control" placeholder="Contraseña">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-default">Registrar</button>
+        <div style="clear:both"></div>
+      </div>
+      <div class="page-header page-header-custom" ></div>
+      <div class="form-group">
+        <input name="nombre" class="form-control" placeholder="Nombre">
+      </div>
+      <div class="form-group">
+        <input name="apellido" class="form-control" placeholder="Apellido">
+      </div>
+      <div class="form-group">
+        <input name="direccion" class="form-control" placeholder="Dirección">
+      </div>
+      <div class="form-group">
+        <input name="telefono" class="form-control" placeholder="Teléfono">
+      </div>
+      <div class="form-group">
+        <input name="email" class="form-control" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="btn btn-default pull-right">Registrar</button>
+      </div>
+    </form>-->
